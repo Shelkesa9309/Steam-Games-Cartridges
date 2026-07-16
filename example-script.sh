61f4164350981c4eab2c6f645040b3bb8626603b
LewdM3at <angkunster@hotmail.com>
2026-07-16 15:11:37 +0200

    added example script

---
+++ example-script.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+##########################################################################################
+#   Edit the actions here. What should happen after the Cartridge has been plugged in    #
+##########################################################################################
+
+# Navigate to game's page
+steam steam://nav/games/details/1091500
+
+# Runs the game, will be installed if necessary
+steam steam://run/1091500
+
+# Same as run, but with support for mods and non-Steam shortcuts.
+#steam steam://rungameid/1091500
+
+# Same as run, but with support for multiple launch options.
+#steam steam://launch/1091500
+
+# For other Steam URL Protocol commands check the documentation:
+# https://developer.valvesoftware.com/wiki/Steam_browser_protocol
+
